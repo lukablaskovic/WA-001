ec4cdb13195b381d0fc58de130bc6afc20cadc09
lukablaskovic <lblaskovi@unipu.hr>
2022-03-16 16:48:19 +0100

    dovrsen zadatak1

---
+++ src/components/Details.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card class="mx-auto mt-12" max-width="400">
+    <v-img class="black--text align-end" height="200px" :src="authorImage">
+    </v-img>
+    <v-card-title>{{ authorName }}</v-card-title>
+    <v-card-subtitle class="pb-0"> {{ authorEmail }} </v-card-subtitle>
+    <br />
+    <v-card-text class="text--primary">
+      <div>{{ msg }}</div>
+    </v-card-text>
+    <v-card-subtitle class="pb-0"
+      >Objavljeno: {{ date.slice(0, 10) }}
+    </v-card-subtitle>
+    <v-card-actions>
+      <v-btn @click.native="$router.push('/')" color="orange" text>
+        Povratak
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  data: () => ({
+    sha: this.$route.params.sha,
+  }),
+  props: {
+    authorName: String,
+    authorEmail: String,
+    msg: String,
+    date: Date,
+    authorImage: String,
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="300" tile>
+    <v-card class="mx-auto mt-12" max-width="320" tile>
       <v-list dense>
         <v-subheader>Prikaz commitova (SHA)</v-subheader>
         <v-list-item-group v-model="selectedItem" color="primary">
-          <v-list-item v-for="(item, i) in data" :key="i">
-            <v-list-item-content>
-              <v-list-item-title v-text="item.sha"></v-list-item-title>
-            </v-list-item-content>
+          <v-list-item v-for="(item, i) in data.slice(-10 + p, 0 + p)" :key="i">
+            <router-link
+              :to="{
+                name: 'details',
+                params: {
+                  sha: item.sha,
+                  authorName: item.commit.author.name,
+                  authorEmail: item.commit.author.email,
+                  msg: item.commit.message,
+                  date: item.commit.author.date,
+                  authorImage: item.author.avatar_url,
+                },
+              }"
+            >
+              <v-list-item-content>
+                <v-list-item-title v-text="item.sha"></v-list-item-title>
+              </v-list-item-content>
+            </router-link>
           </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-card>
+    <div class="text-center">
+      <v-pagination v-model="page" :length="3" circle></v-pagination>
+    </div>
   </div>
 </template>
 
@@ -27,7 +44,13 @@ export default {
   },
   data: () => ({
     selectedItem: -1,
-    data: null,
+    page: 1,
+    data: [],
   }),
+  computed: {
+    p() {
+      return this.page * 10;
+    },
+  },
 };
 </script>
