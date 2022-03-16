aa2bba481427276b48d197b1b506227de83c63d2
lukablaskovic <lblaskovi@unipu.hr>
2022-03-16 13:48:39 +0100

    napravljen get data sa axios, spremanje vrijednosti i prikaz preko vuetify v-carda

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,33 @@
 <template>
-  <hello-world />
+  <div>
+    <v-card class="mx-auto" max-width="300" tile>
+      <v-list dense>
+        <v-subheader>Prikaz commitova (SHA)</v-subheader>
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item v-for="(item, i) in data" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-text="item.sha"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+import axios from "axios";
+export default {
+  name: "Home",
+  async created() {
+    const response = await axios.get(
+      "https://api.github.com/repos/vuejs/vue/commits"
+    );
+    this.data = response.data;
+  },
+  data: () => ({
+    selectedItem: -1,
+    data: null,
+  }),
+};
 </script>
